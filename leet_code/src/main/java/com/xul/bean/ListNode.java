31d4ad7266a425549631a03922dc4726247f1715
xul <513624876@qq.com>
2022-08-15 17:13:18 +0800

    从今天开始重新做题

--- leet_code/src/main/java/com/xul/bean/ListNode.java
+++ leet_code/src/main/java/com/xul/bean/ListNode.java
@@ -1,14 +1,21 @@
 package com.xul.bean;
 
 /**
- * Created by lxu on 2020/9/4.
+ * Created by lxu on 2022/8/15.
  */
 public class ListNode {
     public int val;
     public ListNode next;
 
-    public ListNode(int x) {
-        val = x;
+    public ListNode() {
     }
 
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 }
